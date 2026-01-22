c3e7aceed2dc5ed8a9a13dac9ea9f77ca920eb8d
dfirkhaled <dfir.khaled@gmail.com>
2026-01-22 14:02:05 +0200

    Add files via upload

---
+++ Implement_Function_1.cpp
@@ -0,0 +1,27 @@
+/*
+
+Implement Function Number 1
+Created In 1/22/2026
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int Count(char Sub_String, string String) {
+    int Result = 0;
+    for (int i = 0; i < String.size(); i++) {
+        if(Sub_String == String[i]) {
+            Result++;
+        }
+    }
+    cout << "Result: " << Result;
+    return Result;
+}
+
+
+// int main() {
+//     Count('Your Char', "Your String");
+// }
